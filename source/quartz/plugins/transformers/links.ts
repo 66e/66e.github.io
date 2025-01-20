5edb312498c291294a354e2f42ce54627f28992d
66e <34702774+66e@users.noreply.github.com>
2025-01-20 13:36:52 +0800

    iIsInternal

--- source/quartz/plugins/transformers/links.ts
+++ source/quartz/plugins/transformers/links.ts
@@ -56,12 +56,33 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                 typeof node.properties.href === "string"
               ) {
                 let dest = node.properties.href as RelativeURL
+                console.log(dest);
                 const classes = (node.properties.className ?? []) as string[]
                 const isExternal = isAbsoluteUrl(dest)
                 classes.push(isExternal ? "external" : "internal")
 
                 if (isExternal && opts.externalLinkIcon) {
                   node.children.push({
+
+                    type: "element",
+                    tagName: "span",
+                    properties: {
+                      "aria-hidden": "true",
+                      class: "external-icon",
+                      style: "max-width:0.8em;max-height:0.8em",
+                      viewBox: "0 0 512 512",
+                    },
+                    children: [
+                      {
+                        type: "element",
+                        tagName: "pre",
+                        properties: {
+                          d: "M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z",
+                        },
+                        children: [],
+                      },
+                    ],
+
                     type: "element",
                     tagName: "svg",
                     properties: {
@@ -85,7 +106,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                     tagName: "a",
                     properties: {
                       "aria-hidden": "true",
-                      class: "external-icon",
+                      class: dest,
                       style: "max-width:0.8em;max-height:0.8em",
                       viewBox: "0 0 512 512",
                     },
@@ -94,7 +115,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                         type: "element",
                         tagName: "img",
                         properties: {
-                          src: "https://66e.github.io/AA1xrpf6.png",
+                          src: dest,
                         },
                         children: [],
                       },
@@ -119,7 +140,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
 
                 // don't process external links or intra-document anchors
                 const isInternal = !(isAbsoluteUrl(dest) || dest.startsWith("#"))
-                //if (isInternal) {
+                if (isInternal) {
                   dest = node.properties.href = transformLink(
                     file.data.slug!,
                     dest,
@@ -140,7 +161,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                   const simple = simplifySlug(full)
                   outgoing.add(simple)
                   node.properties["data-slug"] = full
-                //}
+                }
 
                 // rewrite link internals if prettylinks is on
                 if (
