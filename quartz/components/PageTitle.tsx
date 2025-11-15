f985dea48af6ca3965aeca2d7fbd737ea74bf6bc
66e <34702774+66e@users.noreply.github.com>
2025-11-15 20:36:11 +0800

    h2-button

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,7 +7,9 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <button id="btnb">Click me</button>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>{title}</a>
+    </h2><button id="btnn">Click me</button>
   )
 }
 
@@ -18,13 +20,12 @@ PageTitle.css = `
   font-family: var(--titleFont);
 }
 `
-
 PageTitle.beforeDOMLoaded = `
   console.log("hello from before the page loads!")
 `
  
 PageTitle.afterDOMLoaded = `
-document.getElementById('btnb').onclick = () => {
+document.getElementById('btnn').onclick = () => {
   alert('button clicked!')
 }
 `
