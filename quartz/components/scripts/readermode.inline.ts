7bbfc752b654139ef1d4a5cffc1feb7eab5684c5
66e <34702774+66e@users.noreply.github.com>
2025-11-29 17:11:49 +0800

    readermode-inline-og-title

--- quartz/components/scripts/readermode.inline.ts
+++ quartz/components/scripts/readermode.inline.ts
@@ -7,7 +7,7 @@ const emitReaderModeChangeEvent = (mode: "on" | "off") => {
   document.dispatchEvent(event)
 }
 
-document.addEventListener("nav", () => {
+document.addEventListener("nav", (e) => {
   const switchReaderMode = () => {
     isReaderMode = !isReaderMode
     const newMode = isReaderMode ? "on" : "off"
@@ -22,4 +22,7 @@ document.addEventListener("nav", () => {
 
   // Set initial state
   document.documentElement.setAttribute("reader-mode", isReaderMode ? "on" : "off")
+
+  const currentSlug = e.detail.url
+  console.log( currentSlug )
 })
