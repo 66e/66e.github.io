5e814f9f8be211ab5cc8b54d83f9432f2277538c
66e <34702774+66e@users.noreply.github.com>
2025-12-02 15:04:18 +0800

    switch-true-case-tru-return

--- quartz/plugins/transformers/a_referJS.ts
+++ quartz/plugins/transformers/a_referJS.ts
@@ -4,10 +4,9 @@ export const A_referJS: QuartzTransformerPlugin = () => {
     return {
         name: "A_referJS",
         externalResources() {
-      switch (0) {
-        case 0:
+      switch (true) {
+        case true:
           return {
-            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
             js: [
               {
                 // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
