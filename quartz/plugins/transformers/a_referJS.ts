b5d86dd97fd621fdaad9b353229545c924491685
66e <34702774+66e@users.noreply.github.com>
2025-12-02 15:25:02 +0800

    hacking-To-The-Gate-fastly

--- quartz/plugins/transformers/a_referJS.ts
+++ quartz/plugins/transformers/a_referJS.ts
@@ -1,21 +1,20 @@
 import { QuartzTransformerPlugin } from "../types"
 
 export const A_referJS: QuartzTransformerPlugin = () => {
-    return {
-        name: "A_referJS",
-        externalResources() {
-
-          return {
+  return {
+    name: "A_referJS",
+    externalResources() {
+        return {
+            css: [{ content: "https://fastly.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
             js: [
               {
                 // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-                src: "https://66e.github.io/6/navAttach.js",
+                src: "https://fastly.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
                 loadTime: "afterDOMReady",
                 contentType: "external",
               },
             ],
           }
-
     },
-    }
+  }
 }
