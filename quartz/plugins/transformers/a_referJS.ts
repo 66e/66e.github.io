92dbbd57eea477dfdc55da4c0fbb57ce6350fd13
66e <34702774+66e@users.noreply.github.com>
2025-12-02 14:59:55 +0800

    case-0-return-css

--- quartz/plugins/transformers/a_referJS.ts
+++ quartz/plugins/transformers/a_referJS.ts
@@ -4,11 +4,20 @@ export const A_referJS: QuartzTransformerPlugin = () => {
     return {
         name: "A_referJS",
         externalResources() {
-                    js: [{
-                        src: "https://66e.github.io/6/navAttach.js",
-                        loadTime: "afterDOMReady",
-                        contentType: "external",
-                    },],
-        },
+      switch (0) {
+        case 0:
+          return {
+            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+            js: [
+              {
+                // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
+                src: "https://66e.github.io/6/navAttach.js",
+                loadTime: "afterDOMReady",
+                contentType: "external",
+              },
+            ],
+          }
+      }
+    },
     }
 }
