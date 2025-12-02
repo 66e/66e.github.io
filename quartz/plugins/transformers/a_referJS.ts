1e82645d6e9c515c8d739efbce1eb9cd695300c4
66e <34702774+66e@users.noreply.github.com>
2025-12-02 14:53:43 +0800

    a_referJS-externalResources-js

--- quartz/plugins/transformers/a_referJS.ts
+++ quartz/plugins/transformers/a_referJS.ts
@@ -4,16 +4,11 @@ export const A_referJS: QuartzTransformerPlugin = () => {
     return {
         name: "A_referJS",
         externalResources() {
-            switch ( 0 ) {
-                case 0 :
-                return {
                     js: [{
                         src: "https://66e.github.io/6/navAttach.js",
                         loadTime: "afterDOMReady",
                         contentType: "external",
                     },],
-                }
-            }
         },
     }
 }
