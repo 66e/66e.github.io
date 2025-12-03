7ea9683e5ed40d712d53f1788894a3eec40dad8e
66e <34702774+66e@users.noreply.github.com>
2025-12-03 14:29:56 +0800

    slug-endsWith-_JS9-noSharp

--- content/6/navAttach.js
+++ content/6/navAttach.js
@@ -1,17 +1,13 @@
 document.addEventListener("nav", ( e ) => {
-    const matches = document.querySelectorAll("a[data-slug='tags/6/jsAttached']")
-    if ( matches.length > 0) {
-        const currentSlug = e.detail.url
+    const slug = e.detail.url
+    if ( slug.endsWith("_JS9") ) {
         const script = document.createElement("script")
-        script.src = "../" + currentSlug + ".js"
-        const script2 = document.createElement("script")
-        script2.src = "https://raw.githubusercontent.com/66e/66e.github.io/refs/heads/v4/content/"
-         + currentSlug + ".md"
+        script.src = "https://raw.githubusercontent.com/66e/66e.github.io/refs/heads/v4/content/"
+        + slug + ".md"
         const elem = document.querySelector("article.popover-hint")
         if ( elem ) {
             elem.appendChild( script )
-            elem.appendChild( script2 )
         }
-        console.log( ".js" + Date.now() );
+        console.log( slug + ".js" + Date.now() );
     }
 })
