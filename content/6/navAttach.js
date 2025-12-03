6cc5a08166a34019d3c60925a22fac13fe78f96e
66e <34702774+66e@users.noreply.github.com>
2025-12-03 11:55:33 +0800

    waveSurfer_JS9-md-navAttach

--- content/6/navAttach.js
+++ content/6/navAttach.js
@@ -4,9 +4,13 @@ document.addEventListener("nav", ( e ) => {
         const currentSlug = e.detail.url
         const script = document.createElement("script")
         script.src = "../" + currentSlug + ".js"
+        const script2 = document.createElement("script")
+        script2.src = "https://raw.githubusercontent.com/66e/66e.github.io/refs/heads/v4/content/"
+         + currentSlug + ".md"
         const elem = document.querySelector("article.popover-hint")
         if ( elem ) {
             elem.appendChild( script )
+            elem.appendChild( script2 )
         }
         console.log( ".js" + Date.now() );
     }
