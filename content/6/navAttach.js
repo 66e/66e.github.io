b7a786e2ef9d6f2d34b941946d089534003a1a8e
66e <34702774+66e@users.noreply.github.com>
2025-12-04 15:34:36 +0800

    navAttach-async-url-slug-slice-js

--- content/6/navAttach.js
+++ content/6/navAttach.js
@@ -1,13 +1,18 @@
 document.addEventListener("nav", ( e ) => {
     const slug = e.detail.url
-    if ( slug.endsWith("_JS9") ) {
-        const script = document.createElement("script")
-        script.src = "https://raw.githubusercontent.com/66e/66e.github.io/refs/heads/v4/content/"
-        + slug + ".md"
-        const elem = document.querySelector("article.popover-hint")
-        if ( elem ) {
-            elem.appendChild( script )
+    if ( slug.endsWith( "_JS9" ) ) {
+        const loadModule = ( moduleUrl ) => {
+            const module = import ( moduleUrl );
+            return module;
         }
-        console.log( slug + ".js" + Date.now() );
+
+        ( async () => {
+            const url = "../" + slug.slice(0, 4) + ".js";
+            try {
+                const module = await loadModule ( url );
+                } catch (error) {
+                    console.error(error);
+                }
+        }) ();
     }
 })
