dfb3b931a5e8ded1b2530ed763c6096ff620298a
66e <34702774+66e@users.noreply.github.com>
2025-12-08 18:19:07 +0800

    export-default-async-f-initLyricsP

--- content/9/bAldLyricS-04.js
+++ content/9/bAldLyricS-04.js
@@ -1,13 +1,17 @@
 import initPlayer from "./APlayerLyricsModule.js";
 
-const targetElem = document.querySelector( "article.popover-hint" ) ||
-document.querySelector( "div.markdown-body" ) || document.body;
+export default async function initLyricsPage() {
+    const targetElem =
+        document.querySelector("article.popover-hint") ||
+        document.querySelector("div.markdown-body") ||
+        document.body;
 
-initPlayer({
-    mount: targetElem,     // 安装到哪里
-    audio: "https://oss.mojidict.com/article/audio/dd16f7f0-8367-4d49-830a-3a66d0489982.mp3",        // 音频地址
-    lrc: "https://66e.github.io/9/%E3%83%A9%E3%82%A4%E3%82%A2.lrc", 
-    cover: "https://zweima.com/wp/wp-content/uploads/b2b99ccc4fe2b7d9e69f2b14b16b7a2e-1024x1024.jpg",       // LRC 地址
-    title: "ライア",
-    artist: "Zwei"
-});
+    return initPlayer({
+        mount: targetElem,
+        audio: "https://oss.mojidict.com/article/audio/dd16f7f0-8367-4d49-830a-3a66d0489982.mp3",
+        lrc: "https://66e.github.io/9/%E3%83%A9%E3%82%A4%E3%82%A2.lrc",
+        cover: "https://zweima.com/wp/wp-content/uploads/b2b99ccc4fe2b7d9e69f2b14b16b7a2e-1024x1024.jpg",
+        title: "ライア",
+        artist: "Zwei",
+    });
+}
